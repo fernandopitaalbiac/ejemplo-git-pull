13d53b2d18bc98a5c9cc4c262ae75d1a8a436a3e
fernandopitaalbiac <72222256+fernandopitaalbiac@users.noreply.github.com>
2023-09-16 18:48:32 +0200

    Update ClaseJava.java
    Cambios en el archivo para probar git fetch

--- ClaseJava.java
+++ ClaseJava.java
@@ -2,9 +2,20 @@
 
 public class ClaseJava{
 
+  public static int suma(int a, int b){
+
+    return a+b;
+  }
+
   public static void main(String[] args){
 
     System.out.println("Cambios introducidos en el repositorio remoto");
+
+    int a =1000;
+    int b =2*a;
+    int c = suma(a,b);
+
+    System.out.println("El resultado es "+c);
   }
 
 }
